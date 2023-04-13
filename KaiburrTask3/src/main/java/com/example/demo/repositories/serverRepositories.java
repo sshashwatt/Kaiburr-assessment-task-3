d7f6fd5591e96f67edc04651c68a05781dd8ae38
sshashwatt <92125458+sshashwatt@users.noreply.github.com>
2023-04-13 21:29:43 +0530

    Delete serverRepositories.java

--- KaiburrTask3/src/main/java/com/example/demo/repositories/serverRepositories.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.demo.model.Servers;
-
-public interface serverRepositories extends MongoRepository<Servers,Integer>{
-	
-}
